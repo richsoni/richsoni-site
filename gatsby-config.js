6d7d20105627324ee5a37db989df62ecf8323de2
Rich Soni <rich@richsoni.com>
2018-08-24 11:15:34 -0400

    some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ module.exports = {
     resolve: 'gatsby-plugin-typography',
     options: {
       pathToConfigModule: 'src/utils/typography.js',
-    }
+    },
   },
-  'gatsby-plugin-react-helmet'],
-};
+  'gatsby-plugin-react-helmet',
+  'gatsby-transformer-remark',
+]};
